aac65f11131b2a1a01dc3f002099c56a9d6b054c
GlenwoodSoftWareDevClub <glenwoodsoftwaredevclub@gmail.com>
2025-10-08 19:32:10 -0500

    Update script.js
    change code to work with members page

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-const togglethemeBtn = document.getElementById("toggleTheme");
+const toggleBtn = document.getElementById("toggleTheme") || document.getElementById("toggleTheme2");
 
-if (localStorage.getItem("theme") === "dark") {
-    document.body.classList.add("dark");
-}
+if (toggleBtn) {
 
-togglethemeBtn.addEventListener("click", () => {
+  toggleBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark");
 
-if (document.body.classList.contains("dark")) {
-    localStorage.setItem("theme", "dark");
-} else {
-    localStorage.setItem("theme", "light")
-}
-});
+    if (document.body.classList.contains("dark")) {
+      localStorage.setItem("theme", "dark");
+    } else {
+      localStorage.setItem("theme", "light");
+    }
+  });
 
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+  }
+}
